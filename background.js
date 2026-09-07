058312854109b966e754b8d521f3dbbd0b73f7fc
Hoang Ninh <ninhvanhoang0207@gmail.com>
2026-09-07 22:31:18 +0900

    first commit

---
+++ background.js
@@ -0,0 +1,118 @@
+let lastProcessedUrl = ""; // Lưu URL gần nhất đã xử lý thành công
+var subTitleLoaded = false;
+const urlSkipList = [];
+var beforeRequestCount = 0;
+var onCompletedCount = 0;
+var isFetchedMetadata = false;
+var metaDataUrl = null;
+var movieID = null;
+
+chrome.webRequest.onBeforeRequest.addListener(
+    async (details) => {
+        const currentUrl = details.url;
+         if (currentUrl.includes("/metadata?") && details.type === "xmlhttprequest") {
+            if (metaDataUrl === currentUrl) return;
+            console.log("Đang tải metadata:", currentUrl);
+            metaDataUrl = currentUrl;
+            if (details.tabId !== -1) { // Kiểm tra nếu tabId hợp lệ
+                await fetchMetadata(currentUrl, details.tabId);
+            }
+        }
+
+        if (currentUrl.includes("/?o=") && details.type === "xmlhttprequest") {
+            if (subTitleLoaded) return;
+            if (details.tabId !== -1) { // Kiểm tra nếu tabId hợp lệ
+                await fetchSubtitleContent(currentUrl, details.tabId);
+            }
+        }
+    },
+    { urls: ["*://*.nflxvideo.net/*", "*://*.netflix.com/nq/website/memberapi/release/metadata*"] }
+);
+
+// Lắng nghe yêu cầu từ Content Script
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request.type === "FETCH_SUBTITLE_RAW") {
+        fetchSubtitleContent(request.url, sender.tab.id).then(result => {
+            sendResponse(result);
+        });
+
+        return true; // Bắt buộc trả về true để giữ kết nối không đồng bộ
+    }
+});
+
+chrome.webRequest.onCompleted.addListener(
+    async function(details) {
+        const currentUrl = details.url;
+        // Kiểm tra nếu là URL phụ đề của Netflix
+
+        if (currentUrl.includes("/?o=") && details.statusCode === 200 && details.type === "xmlhttprequest") {
+            urlSkipList.push(currentUrl);
+            if (subTitleLoaded) {
+                // Load subtitle
+                if (details.tabId !== -1) { // Kiểm tra nếu tabId hợp lệ
+                    await fetchSubtitleContent(currentUrl, details.tabId); // Chấp nhận tải lại phụ đề lúc đầu tiên
+                }
+            }
+        }
+    },
+    { urls: ["*://*.netflix.com/*",  "*://*.nflxvideo.net/*"] }
+);
+
+async function fetchMetadata(url, tabId) {
+    try {
+        const response = await fetch(url);
+        const jsonData = await response.json();
+
+        isFetchedMetadata = true;
+        movieID = (jsonData && jsonData.video) ? (jsonData.video.currentEpisode ? jsonData.video.currentEpisode : jsonData.video.id) : null; // Lưu movieId từ metadata
+        console.log("Metadata đã được tải:", {"currentEpisode": jsonData.video.currentEpisode, "MovieID": jsonData.video.id});
+
+    } catch (error) {
+        console.error("Lỗi khi tải metadata:", error);
+    }
+}
+
+async function fetchSubtitleContent(url, tabId) {
+    try {
+        const response = await fetch(url);
+        const xmlText = await response.text();
+
+        subTitleLoaded = isFullSubTitleData(xmlText.toString());
+        console.log("Phụ đề đã được tải:", subTitleLoaded, "MovieID:", movieID);
+        if (subTitleLoaded) {
+            chrome.tabs.sendMessage(tabId, {
+                    type: "SUBTITLE_RAW_XML",
+                    xml: xmlText,
+                    meta_data: { url: url, movieId: movieID, language: currentLanguageFromXML(xmlText) }
+            });
+        }
+        // const movieId = movieIdFromXML(xmlText);
+        // const currentLang = currentLanguageFromXML(xmlText);
+
+        return true;
+        
+    } catch (error) {
+        console.error("Lỗi khi tải phụ đề:", error);
+    }
+}
+
+const isFullSubTitleData = (xmlString) => {
+    const match = xmlString.match(/nttm:textType="([^"]+)"/);
+    // if (match) {
+    //     console.log(match[1]); // Kết quả: "CC"
+    // }
+
+    return match ? (match[1] === "CC" || match[1] === "SUBS") : false;
+}
+
+const movieIdFromXML = (xmlString) => {
+    const id = xmlString.match(/nttm:movieID="([^"]+)"/);
+
+    return id ? id[1] : null;
+}
+
+const currentLanguageFromXML = (xmlString) => {
+    const lang = xmlString.match(/xml:lang="([^"]+)"/);
+
+    return lang ? lang[1] : null;
+}
---
+++ content.js
@@ -0,0 +1,357 @@
+const openSubtitleUI = () => {
+    const langPopup = document.getElementById('lang-popup');
+    if (langPopup) {
+        langPopup.style.display = 'block';
+    }
+}
+
+const closeSubtitleUI = () => {
+      const langPopup = document.getElementById('lang-popup');
+      if (langPopup) {
+        langPopup.style.display = 'none';
+    }
+};
+
+const cleanSubTitle = () => {
+    mockSubtitles = []
+}
+
+// Khởi tạo UI khi nội dung được tải
+// initExtensionUI();
+
+function initExtensionUI() {
+    // 1. Tìm container của Netflix
+    const videoContainer = document.querySelector('.watch-video') || document.body;
+
+    // 2. Tạo nút Settings cố định (nếu chưa có)
+    console.log("Khởi tạo UI phụ đề, videoContainer:", videoContainer);
+    if (!document.getElementById('netflix-sub-settings-btn')) {
+        console.log("Tạo nút Settings cho phụ đề...");
+        const settingsBtn = document.createElement('div');
+        settingsBtn.id = 'netflix-sub-settings-btn';
+        settingsBtn.innerHTML = '⚙️';
+        videoContainer.appendChild(settingsBtn);
+
+        // Tạo Popup đi kèm nút Settings
+        const langPopup = document.createElement('div');
+        langPopup.id = 'lang-popup';
+        langPopup.innerHTML = `
+            <div class="popup-header">
+                <span>🛠️ Cấu hình </span>
+            </div>
+            <div class="popup-body">
+            <div style="display: flex; align-items: center; margin-top: 10px;">
+                <input type="checkbox" id="toggle-sub-visibility" checked style="margin-right: 8px;">
+                <label for="toggle-sub-visibility" style="font-size: 12px; color: #ccc;">Hiển thị phụ đề</label>
+            </div>
+                <label for="lang-select">Ngôn ngữ</label>
+                <div class="select-wrapper">
+                    <select id="lang-select">
+                        <option value="">-- Chọn --</option>
+                    </select>
+                </div>
+                <button id="apply-lang-btn">
+                    <span>Áp dụng</span>
+                </button>
+            </div>
+        `;
+        videoContainer.appendChild(langPopup);
+
+        // Sự kiện đóng/mở popup
+        settingsBtn.onclick = (e) => {
+            e.stopPropagation();
+            const isHidden = langPopup.style.display === 'none' || langPopup.style.display === '';
+            if (isHidden) {
+                openSubtitleUI();
+            } else {
+                closeSubtitleUI();
+            }
+        };
+
+        // Đóng popup khi click ra ngoài
+        const toggleBtn = document.getElementById('toggle-sub-visibility');
+        toggleBtn.addEventListener('change', (e) => {
+            if (e.target.checked) {
+                document.getElementById('lang-popup').style.display = 'none';
+                handleApplySubtitle();
+            } else {
+                console.log("Ẩn phụ đề");
+                cleanSubTitle();
+                document.getElementById('lang-popup').style.display = 'none';
+            }
+        });
+
+        // Xử lý khi nhấn nút Áp dụng
+        const applyBtn = langPopup.querySelector('#apply-lang-btn');
+        const langSelect = langPopup.querySelector('#lang-select');
+
+        applyBtn.onclick = () => {
+            const selectedUrl = langSelect.value;
+            if (selectedUrl) {
+                fetchFullSubtitle(selectedUrl);
+            }
+
+            closeSubtitleUI();
+        };
+    }
+
+    // 3. Tạo khung hiển thị phụ đề (Vẫn giữ tính năng kéo thả)
+    createSubtitleDisplay(videoContainer);
+}
+
+function createSubtitleDisplay(parent) {
+    if (document.getElementById('subtitle-container')) { // Nếu đã tồn tại, không tạo lại
+        return;
+    } 
+
+    const container = document.createElement('div');
+    container.id = 'subtitle-container';
+    container.innerText = "Phụ đề thứ 2 sẽ hiển thị ở đây...";
+    parent.appendChild(container);
+
+    // Khôi phục vị trí/kích thước cũ từ storage
+    chrome.storage.local.get(['subConfig'], (res) => {
+        if (res.subConfig) {
+            container.style.top = res.subConfig.top;
+            container.style.left = res.subConfig.left;
+            container.style.width = res.subConfig.width;
+            container.style.transform = 'none';
+            console.log("Đã khôi phục cấu hình phụ đề:", res.subConfig);
+        }
+    });
+
+    makeElementDraggable(container);
+}
+
+
+function saveSubtitleConfig(elmnt) {
+    const config = {
+        top: elmnt.style.top,
+        left: elmnt.style.left,
+        width: elmnt.style.width,
+    };
+    console.log("Lưu cấu hình phụ đề:", config);
+    chrome.storage.local.set({ subConfig: config });
+}
+
+function makeElementDraggable(elmnt) {
+    let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+
+    elmnt.onmousedown = (e) => {
+        // Không cho phép kéo nếu người dùng đang nhấn vào góc resize
+        const rect = elmnt.getBoundingClientRect();
+        if (e.clientX > rect.right - 20 && e.clientY > rect.bottom - 20) return;
+
+        e = e || window.event;
+
+        e.preventDefault();
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = () => {
+            document.onmouseup = null;
+            document.onmousemove = null;
+            saveSubtitleConfig(elmnt); // Lưu sau khi thả chuột
+        };
+        document.onmousemove = (e) => {
+            e.preventDefault();
+            pos1 = pos3 - e.clientX;
+            pos2 = pos4 - e.clientY;
+            pos3 = e.clientX;
+            pos4 = e.clientY;
+            
+            elmnt.style.transform = 'none'; // Xóa căn giữa mặc định khi bắt đầu kéo
+            elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+            elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+            elmnt.style.bottom = 'auto'; // Quan trọng: bỏ bottom để top có hiệu lực
+        };
+    };
+}
+
+
+
+// Lắng nghe danh sách ngôn ngữ từ Background gửi về
+chrome.runtime.onMessage.addListener((msg) => {
+    if (msg.type === "UPDATE_LANGUAGE_LIST") {
+        const langSelect = document.getElementById('lang-select');
+        if (!langSelect) return;
+        console.log("Lang list received:", langSelect, msg.tracks);
+
+        langSelect.innerHTML = '<option value="">-- Chọn ngôn ngữ --</option>'; 
+        
+        msg.tracks.forEach(track => {
+            const option = document.createElement('option');
+            option.value = track.url;
+            option.innerText = track.label;
+            langSelect.appendChild(option);
+        });
+    }
+});
+
+var mockSubtitles = [];
+// Hàm cập nhật phụ đề
+var movieID = null;
+function updateSubtitles() {
+    if (!document.getElementById('subtitle-container')) { // Fix bug exit movie and change to the another
+        const watchVideoContainer = document.querySelector('.watch-video') || document.body;
+        createSubtitleDisplay(watchVideoContainer);
+    };
+
+    const subtitleElement = document.getElementById('subtitle-container');
+    const videoContainer = document.querySelector('video');
+
+    if (!videoContainer) return; // Wait for video element to load
+
+    // Định vị phụ đề theo vị trí của video trên màn hình
+    if (!subtitleElement.style.top && !subtitleElement.style.left) {
+        const rect = videoContainer.getBoundingClientRect();
+        subtitleElement.style.top = `${rect.top + rect.height * 0.8}px`;
+        subtitleElement.style.left = `${rect.left + rect.width / 2}px`;
+        subtitleElement.style.transform = "translateX(-50%)";
+    }
+
+    const currentTime = videoContainer.currentTime;
+    const currentSub = mockSubtitles.find(s => currentTime >= s.begin && currentTime <= s.end);
+
+    if (currentSub) {
+        subtitleElement.innerText = currentSub.text;
+        subtitleElement.style.display = 'block';
+    } else {
+        subtitleElement.style.display = 'none';
+    }
+}
+
+const convertTicks = (ticksStr) => {
+    const ticks = parseInt(ticksStr.replace('t', ''));
+    const totalSeconds = ticks / 10000000;
+
+    return totalSeconds;
+}
+
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (message.type === "SUBTITLE_RAW_XML") {
+        const parser = new DOMParser();
+        const xmlDoc = parser.parseFromString(message.xml, "text/xml");
+        
+        // Lấy tất cả các thẻ <p> (đại diện cho các dòng phụ đề)
+        const paragraphs = xmlDoc.getElementsByTagName("p");
+        const rawSubtitles = [];
+        const contentSrt = [];
+
+        var count = 1;
+        mockSubtitles = [];
+        for (let p of paragraphs) {
+            const begin = p.getAttribute("begin"); // Ví dụ: "462962499t"
+            const end = p.getAttribute("end");
+            const text = p.textContent; // Nội dung câu thoại
+            const subTitle = {
+                id: count,
+                begin: convertTicks(begin),
+                end: convertTicks(end),
+                text: text
+            }
+
+            rawSubtitles.push(subTitle);
+
+             // Tạo định dạng SRT
+            const beginTime = convertTicks(begin);
+            const endTime = convertTicks(end);
+            const beginFormatted = new Date(beginTime * 1000).toISOString().substr(11, 12);
+            const endFormatted = new Date(endTime * 1000).toISOString().substr(11, 12);
+
+            contentSrt.push(`${count}\n${beginFormatted} --> ${endFormatted}\n${text}\n`);
+
+            mockSubtitles.push(subTitle);
+
+            count++;
+        }
+
+        updateCupturedSubTitles(message.meta_data);
+        subtitleIntervalId = setInterval(updateSubtitles, 500);
+    }
+});
+
+var currentMovieId = null;
+const capturedSubtitles = [];
+// SubData structure example: {language: "en", url: "https://..., movieId: "12345"}
+const updateCupturedSubTitles = (subData) => {
+    if (currentMovieId && currentMovieId !== subData.movieId) {
+        // Reset subtitle If movieId changed
+        capturedSubtitles.length = 0;
+    }
+
+    currentMovieId = subData.movieId;
+
+    if (!currentMovieId || currentMovieId === "undefined") {
+        console.log("Movie ID không hợp lệ:", currentMovieId);
+        return;
+    }
+
+
+    capturedSubtitles.some(sub => sub.language === subData.language) || capturedSubtitles.push(subData);
+
+    const langSelect = document.getElementById('lang-select');
+    if (!langSelect) {
+        initExtensionUI(); // Khởi tạo UI nếu chưa có
+        console.log("Không tìm thấy thẻ chọn ngôn ngữ.");
+        return;
+    };
+
+    // Giữ lại option mặc định
+    langSelect.innerHTML = '<option value="">-- Chọn --</option>';
+
+    capturedSubtitles.forEach(track => {
+        const option = document.createElement('option');
+        option.value = track.url;
+        option.selected = (track.language === subData.language) ? true : false;
+        option.textContent = track.label || track.language;
+        langSelect.appendChild(option);
+    });
+}
+
+async function handleApplySubtitle() {
+    const langSelect = document.getElementById('lang-select');
+    const selectedUrl = langSelect.value;
+    // console.log("Ngôn ngữ được chọn:", [langSelect.value, selectedUrl]);
+    if (!selectedUrl) {
+        document.getElementById('lang-popup').style.display = 'none';
+        cleanSubTitle();
+        return;
+    };
+
+    console.log("⏳ Đang nhờ Background fetch dữ liệu...", selectedUrl);
+    // Gửi yêu cầu lên background
+    fetchFullSubtitle(selectedUrl);
+}
+
+const fetchFullSubtitle = async (url) => {
+    try {
+        chrome.runtime.sendMessage({
+        type: "FETCH_SUBTITLE_RAW",
+        url: url
+    }, (response) => {
+        // console.log("Đã nhận phản hồi từ Background:", response);
+        if (response) {
+            // console.log("✅ Đã nhận được nội dung phụ đề từ Background");
+            document.getElementById('lang-popup').style.display = 'none';
+        } else {
+            alert("Lỗi khi tải phụ đề: " + response.error);
+        }
+    });
+    } catch (error) {
+        console.error("Lỗi khi fetch phụ đề:", error);
+    }
+}
+
+// Gán sự kiện cho nút Áp dụng trong Popup
+// const toggleBtn = document.getElementById('toggle-sub-visibility');
+
+// toggleBtn.addEventListener('change', (e) => {
+//     if (e.target.checked) {
+//         console.log("Hiển thị phụ đề");
+//         handleApplySubtitle();
+//     } else {
+//         console.log("Ẩn phụ đề");
+//         cleanSubTitle();
+//     }
+// });
